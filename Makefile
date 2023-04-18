5d3a38c63f978e95f590bf16fc20fa71018fa4ae
Jari Haapasaari <haapjari@gmail.com>
2023-04-18 21:45:42 +0300

    init

---
+++ Makefile
@@ -0,0 +1,11 @@
+run:
+	deno run main.ts
+
+dev:
+	deno task dev
+
+test:
+	deno test
+
+bench:
+	deno bench
---
+++ main.ts
@@ -0,0 +1,4 @@
+// @deno-types="npm:@types/express@4"
+import express, { NextFunction, Request, Response } from "npm:express";
+
+console.log("hello world");
